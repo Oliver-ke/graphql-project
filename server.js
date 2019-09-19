d89af7b68fa340458a4b915ede9d1aedf3c2f888
azorji kelechi oliver <kelechioliver96@gmail.com>
2019-09-19 05:18:13 +0100

    fix server port number

--- server.js
+++ server.js
@@ -17,6 +17,8 @@ app.use(
 	})
 );
 
-app.listen(4000, () => {
-	console.log('Server running on port 4000');
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+	console.log(`🚀🚀🚀🚀🚀 Server running on port ${PORT}`);
 });
